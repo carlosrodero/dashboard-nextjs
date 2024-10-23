0c6617e527ec275cbec812efeefccaecfa604b72
Carlos Rodero <developer.carodero@gmail.com>
2024-10-22 21:06:07 -0300

    custom email magic link

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import EmailProvider from 'next-auth/providers/nodemailer'
+import nodemailer from "nodemailer"
 
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '../database'
@@ -20,6 +21,22 @@ export const {
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
+      sendVerificationRequest: (params) => {
+        const { identifier, url, provider, theme } = params
+        const { server, from } = provider;
+        const { host } = new URL(url)
+        const mailOptions = {
+          to: identifier,
+          from,
+          subject: `Sign in to ${host}`,
+          text: text({ url, host }),
+          html: html({ url, host, theme }),
+        };
+
+        // Enviando o e-mail usando Nodemailer
+        const transporter = nodemailer.createTransport(server);
+        return transporter.sendMail(mailOptions);
+      },
     }),
   ],
   events: {
@@ -28,3 +45,56 @@ export const {
     },
   },
 })
+
+function html(params: { url: string, host: string, theme: Theme }) {
+  const { url, host, theme } = params
+
+  const escapedHost = host.replace(/\./g, "&#8203;.")
+
+  const brandColor = theme.brandColor || "#346df1"
+  const color = {
+    background: "#f9f9f9",
+    text: "#444",
+    mainBackground: "#fff",
+    buttonBackground: brandColor,
+    buttonBorder: brandColor,
+    buttonText: theme.buttonText || "#fff",
+  }
+
+  return `
+<body style="background: ${color.background};">
+  <table width="100%" border="0" cellspacing="20" cellpadding="0"
+    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center"
+        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        Sign in to <strong>${escapedHost}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
+                target="_blank"
+                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
+                in</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center"
+        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        If you did not request this email you can safely ignore it.
+      </td>
+    </tr>
+  </table>
+</body>
+`
+}
+
+/** Email Text body (fallback for email clients that don't render HTML, e.g. feature phones) */
+function text({ url, host }: { url: string, host: string }) {
+  return `Sign in to ${host}\n${url}\n\n`
+}
